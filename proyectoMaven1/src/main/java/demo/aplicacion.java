801d30a0e0d479a9c3560fe0ae0be5c1df55042b
Diego <diegortizv31@gmail.com>
2020-10-06 16:50:44 -0500

    commit con maven

---
+++ proyectoMaven1/src/main/java/demo/aplicacion.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class aplicacion {
+    
+    public static void main(String[] args) {
+        String s = "Leo es un pendejo";
+        System.out.println(s);
+        
+    }
+    
+}
